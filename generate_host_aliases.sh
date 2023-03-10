f70e799b71497e180e13d0d44b2864ae9cd8f8a5
Raphaël Dein <raphaeldein@gmail.com>
2023-03-10 14:45:00 +0100

    better repo tree

--- generate_host_aliases.sh
+++ generate_host_aliases.sh
@@ -2,8 +2,8 @@
 
 echo -n Setting aliases for host...
 
-cat git_aliases >> generated_bash_aliases
-cat host_aliases >> generated_bash_aliases
+cat aliases/git_aliases >> generated_bash_aliases
+cat aliases/host_aliases >> generated_bash_aliases
 
 mv generated_bash_aliases ~/.bash_aliases
 
--- generate_spindocker_aliases.sh
+++ generate_spindocker_aliases.sh
@@ -2,8 +2,8 @@
 
 echo -n Setting aliases for spindocker...
 
-cat spin_aliases >> generated_bash_aliases
-cat git_aliases >> generated_bash_aliases
+cat aliases/spin_aliases >> generated_bash_aliases
+cat aliases/git_aliases >> generated_bash_aliases
 
 mv generated_bash_aliases ~/.bash_aliases
 
